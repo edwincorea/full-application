71a52320775836bdcfe842bd1b9365491bd528d9
edwin.corea@gmail.com <edwin.corea@gmail.com>
2016-04-25 12:54:05 -0600

    01-express

--- 01-express/server.js
+++ 01-express/server.js
@@ -5,8 +5,40 @@ var http = require("http"),
     
 const app = express();
 
+app.set("view engine", "jade");
+
+//Middlewares. Next is a callback to the next middleware or finally the request handler.
+//Common middlewares flow:  Browser -> Node -> Express -> M1 -> M2 -> Handler
+//Goes back like this:      Browser <- Node <- Express <- M1 <- M2 <- Handler
+//Order of middlewares is important: 
+//1. Custom Middlewares
+//2. Static middleware
+//3. Authentication middleware
+
+app.use((request, response, next) => {
+    console.log("--- In middleware 1");
+    //response.write("Header"); 
+    next();    
+    console.log("--- Out of middleware 1");
+});
+
+//Serve out static files with Express static middleware
+app.use(express.static("./public"));
+
+app.use((request, response, next) => {
+    console.log("--- In middleware 2");
+    //response.write("Other"); 
+    next();    
+    console.log("--- Out of middleware 2");
+});
+
 app.get("/", (request, response) => {
-    response.end("Hello, World!");     
+    response.end("Hello, World!");
+    console.log("In handler");     
+});
+
+app.get("/home", (request, response) => {
+    response.render("index", {title: "Title!"});
 });
 
 const server = new http.Server(app);
