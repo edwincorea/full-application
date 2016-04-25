660b9afa2e23b70416760800e74dfa22b014085c
Edwin Corea <Edwin Corea>
2016-04-24 20:55:42 -0700

    01-express

---
+++ 01-express/server.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var http = require("http"),
+    express = require("express");
+    
+const app = express();
+
+app.get("/", (request, response) => {
+    response.end("Hello, World!");     
+});
+
+const server = new http.Server(app);
+
+const port = 3000;
+server.listen(port, () => {
+    console.log(`Server up and running on port ${port}`);        
+});
