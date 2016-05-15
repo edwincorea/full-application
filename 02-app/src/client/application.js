b6b42a9cf9247fe2eb2c1423196931a9f8bdb3eb
Edwin Corea <Edwin Corea>
2016-05-14 17:11:00 -0700

    Build pipeline with Webpack

--- 02-app/src/client/application.js
+++ 02-app/src/client/application.js
@@ -1,11 +1,1 @@
-//import $ from "jquery";
-import {test} from "shared/test";
 import "./application.scss";
-
-test();
-
-//$("body h1").html("Hei!!!");
-
-if(module.hot) {
-    module.hot.accept();
-}
