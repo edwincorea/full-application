0a414f7430049e9494167def72222a36c89df760
Edwin Corea <Edwin Corea>
2016-07-25 16:51:54 -0700

    Sorting: animation

--- 02-app/src/client/components/playlist/list.js
+++ 02-app/src/client/components/playlist/list.js
@@ -13,7 +13,7 @@ export class PlaylistListComponent extends ElementComponent {
         this._users = usersStore;
         this.$element.addClass("playlist-list");
     }
-    
+
     _onAttach() {
         const $list = this.$element;
         let itemsMap = {};
@@ -35,11 +35,11 @@ export class PlaylistListComponent extends ElementComponent {
 
         Observable.merge(
             this._playlist.state$.first(),
-            this._playlist.actions$.filter(a => a.type === "list"))            
+            this._playlist.actions$.filter(a => a.type === "list"))
             .componentSubscribe(this, ({state}) => {
                 $list.empty();
                 itemsMap = {};
-                for(let source of state.list) {
+                for (let source of state.list) {
                     const component = new PlaylistItemComponent(source);
                     itemsMap[source.id] = component;
                     component.attach($list);
@@ -61,16 +61,16 @@ export class PlaylistListComponent extends ElementComponent {
             .componentSubscribe(this, ({fromSource, toSource}) => {
                 const fromComponent = itemsMap[fromSource.id];
                 const toComponent = toSource ? itemsMap[toSource.id] : null;
-                this._moveItem(fromComponent, toComponent);                
-            });            
-        
+                this._moveItem(fromComponent, toComponent);
+            });
+
         this._playlist.actions$
             .filter(a => a.type === "remove")
             .componentSubscribe(this, ({source}) => {
                 const component = itemsMap[source.id];
                 this._removeItem(component);
             });
-                
+
 
         // ---------------------
         // Current Item
@@ -78,13 +78,13 @@ export class PlaylistListComponent extends ElementComponent {
         this._playlist.serverTime$
             .componentSubscribe(this, current => {
                 //there's nothing playing
-                if (current.source == null) {                    
+                if (current.source == null) {
                     if (lastComponent != null) {
                         lastComponent.isPlaying = false;
                         lastComponent = null;
                     }
 
-                    return;                        
+                    return;
                 }
 
                 const currentComponent = itemsMap[current.source.id];
@@ -96,13 +96,13 @@ export class PlaylistListComponent extends ElementComponent {
                 if (lastComponent != currentComponent) {
                     if (lastComponent != null)
                         lastComponent.isPlaying = false;
-                    
+
                     lastComponent = currentComponent;
                     currentComponent.isPlaying = true;
 
                     //animation here for scrolling additional item into view, with two additional items' height as padding
-                    const scrollTop = currentComponent.$element.offset().top - 
-                        this.$element.offset().top + 
+                    const scrollTop = currentComponent.$element.offset().top -
+                        this.$element.offset().top +
                         this.$element.scrollTop() -
                         currentComponent.$element.height() * 2;
 
@@ -110,11 +110,11 @@ export class PlaylistListComponent extends ElementComponent {
                 }
 
                 currentComponent.progress = current.progress;
-            });   
+            });
     }
 
     _addItem(component, addAfterComponent) {
-        if(addAfterComponent)
+        if (addAfterComponent)
             addAfterComponent.$element.after(component.$element);
         else
             this.$element.prepend(component.$element);
@@ -122,26 +122,45 @@ export class PlaylistListComponent extends ElementComponent {
         const oldHeight = component.$element.height();
         component.$element
             .addClass("selected")
-            .css({height: 0, opacity: 0})
-            .animate({height: oldHeight, opacity: 1}, 250, () => {
+            .css({ height: 0, opacity: 0 })
+            .animate({ height: oldHeight, opacity: 1 }, 250, () => {
                 component.$element
                     .removeClass("selected")
-                    .css({height: "", opacity: ""});
-            });                    
+                    .css({ height: "", opacity: "" });
+            });
     }
 
     _moveItem(fromComponent, toComponent) {
+        const fromOffsetTop = fromComponent.$element[0].offsetTop;
+        let distance = 0; //amount of pixels that we need to offset the item with
+
+
         if (toComponent) {
+            const toOffsetTop = toComponent.$element[0].offsetTop;
             toComponent.$element.after(fromComponent.$element);
+
+            distance = fromOffsetTop - toOffsetTop;
+            if (toOffsetTop < fromOffsetTop)
+                distance -= fromComponent.$element.height();
         } else {
+            distance = fromOffsetTop;
             this.$element.prepend(fromComponent.$element);
         }
+
+        fromComponent.$element
+            .addClass("moving")
+            .css({ top: distance })
+            .animate({ top: 0 }, 250, () => {
+                fromComponent.$element
+                    .removeClass("moving")
+                    .css({ top: "" });
+            });
     }
 
     _removeItem(component) {
         component.$element
             .addClass("remove")
-            .animate({opacity: 0, height: 0}, 250, () => {
+            .animate({ opacity: 0, height: 0 }, 250, () => {
                 component.detach();
             });
     }
@@ -173,7 +192,7 @@ class PlaylistItemComponent extends ElementComponent {
             $(`<img class="thumb" />`).attr("src", source.thumb));
 
         //details
-        const $details = 
+        const $details =
             $(`<div class="details" />`).append([
                 $(`<span class="title" />`).attr("title", source.title).text(source.title),
                 $(`<time />`).text(moment.duration(source.totalTime, "seconds").format())
@@ -181,8 +200,8 @@ class PlaylistItemComponent extends ElementComponent {
 
         this._$progress = $(`<span class="progress" />`);
         this.$element.append($(`<div class="inner" />`).append([
-            $thumb, 
-            $details, 
+            $thumb,
+            $details,
             this._$progress]));
     }
 }  
